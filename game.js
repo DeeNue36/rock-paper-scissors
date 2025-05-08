f5915a30dc59f11dcc99759f870b81eb10f0d344
Dugiii <dugidemeg@gmail.com>
2025-05-08 14:02:08 -0500

    game logic

--- game.js
+++ game.js
@@ -3,15 +3,44 @@ const rulesBtn = document.querySelector('.rules-btn');
 const closeBtn = document.querySelector('.close-modal-btn');
 const rulesModal = document.querySelector('.modal-container');
 
+const gameLogic = [
+    {
+        id: 1,
+        name: 'rock',
+        beats: 'scissors',
+        // loses: 'paper'
+    },
+    {
+        id: 2,
+        name: 'paper',
+        beats: 'rock',
+        // loses: 'scissors'
+    },
+    {
+        id: 3,
+        name: 'scissors',
+        beats: 'paper',
+        // loses: 'rock'
+    }
+]
+
+const playerChoice = document.querySelectorAll('.choice-btn');
+const gameArea = document.querySelector('.game');
+const resultsArea = document.querySelector('.results');
+const displayResults = document.querySelectorAll('.result');
+// const playerChoiceResult = document.querySelector('.player-choice-result');
+// const houseChoiceResult = document.querySelector('.house-choice-result');
+
+
 //* Show & Hide Rules Modal
 rulesBtn.addEventListener('click', () => {
     rulesModal.classList.toggle('show-modal');
-    // rulesModal.classList.add('show-modal');
+    //OR rulesModal.classList.add('show-modal');
     // rulesModal.classList.remove('hide-modal');
 });
 
 closeBtn.addEventListener('click', () => {
     rulesModal.classList.toggle('show-modal');
-    // rulesModal.classList.add('hide-modal');
+    //OR rulesModal.classList.add('hide-modal');
     // rulesModal.classList.remove('show-modal');
 })
