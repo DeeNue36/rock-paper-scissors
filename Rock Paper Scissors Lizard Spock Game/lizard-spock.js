e905af5c6e400b8bc20c5441be809ebb6e063933
Dugiii <dugidemeg@gmail.com>
2025-05-19 03:01:37 -0500

    toggling display of the modal

---
+++ Rock Paper Scissors Lizard Spock Game/lizard-spock.js
@@ -0,0 +1,19 @@
+//* Get DOM Elements
+const rulesBtn = document.querySelector('.rules-btn');
+const closeBtn = document.querySelector('.close-modal-btn');
+const rulesModal = document.querySelector('.modal-container');
+
+
+
+//* Show & Hide the Rules Modal
+rulesBtn.addEventListener('click', () => {
+    rulesModal.classList.toggle('show-modal');
+    //OR rulesModal.classList.add('show-modal');
+    // rulesModal.classList.remove('hide-modal');
+});
+
+closeBtn.addEventListener('click', () => {
+    rulesModal.classList.toggle('show-modal');
+    //OR rulesModal.classList.add('hide-modal');
+    // rulesModal.classList.remove('show-modal');
+})
